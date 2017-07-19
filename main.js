fc4380f8ff6c4b48ad74d15be3e4bc2dcc9fab11
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-07-19 11:39:38 -0500

    work up till array

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ console.log('main.js')
 
 let say_what = document.querySelector('#output');
 let list_items = document.querySelectorAll('.list-item');
-console.log(list_items);
+// console.log(list_items);
 
 
 
 function handle_click(e){
-    console.log(e);
+    // console.log(e);
     let element_text = e.target.innerHTML;
     say_what.innerHTML = `hmmmmm, ${element_text} you have clicked`
 }
@@ -65,6 +65,33 @@ document.querySelector('#add-border')
         yoda.classList.toggle('bordered');
     });
 
+
+document.querySelector('#wrapper')
+    .addEventListener('click', (e)=>{
+        console.log(e.target);
+        console.log(e.currentTarget)
+
+        // if(e.target.tagName.toLowerCase() === 'li'){
+        //     console.log('You clicked on an li')
+        // }
+        // if (e.target.className === 'list-item'){
+        //     console.log(`You clicked on a ${e.target.className}`)
+        // }
+        if (e.target.className === 'page-title') {
+            console.log(`You clicked on a ${e.target.className}`)
+        }
+    });
+
+
+let quotesArray = [
+    "A long time ago in a galaxy far, far away…",
+    "Use the force, Luke.",
+    "The force is strong with this one.",
+    "Do. Or do not. There is no try.",
+    "Fear is the path to the dark side.",
+    "Someday I will be the most powerful Jedi ever.",
+    "You were the chosen one!"
+];
 
 
 
